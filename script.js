48e9540b9c549db166cf4bb5af6426407144cc6f
johnsonparker91 <136095332+johnsonparker91@users.noreply.github.com>
2023-07-18 10:45:34 -0400

    add loop to create 16x16 div grid and add css elements
    Creating a grid is to be used for the creation of the etch-a-sketch while adding css elements such as flexbox will allow visual to be in a square shape.

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    container.appendChild(content);
+}

4d857fe85b6e5ea48476470c50524c2b1a64e887
johnsonparker91 <136095332+johnsonparker91@users.noreply.github.com>
2023-07-25 12:49:41 -0400

    Add JS and CSS to create 16x16 div grid

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
-    const content = document.createElement('div');
-    content.classList.add('content');
-    container.appendChild(content);
+
+function defRows(side) {
+    
+    for (i = 0; i < (side * side); i++) {
+        let row = document.createElement('div');
+        container.appendChild(row).className = "row";
+        for (j = 0; j < side; j++) {
+            let div = document.createElement('div');
+            row.appendChild(div).className = "grid-div";
+        }
+    }
 }
+
+defRows(16, 16);
